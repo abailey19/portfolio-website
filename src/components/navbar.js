c24300d463ef756ba6aa3d69883cd8d25139f32d
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2020-06-14 18:31:49 -0400

    nav setup and home page

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <Link to="/" className="navlink name">Anne Bailey</Link>
+      <div className="navlinks">
+        <Link to="/" className="navlink" activeClassName="active">Home</Link>
+        <Link to="/projects/" className="navlink" activeClassName="active">Projects</Link>
+        <Link to="/work/" className="navlink" activeClassName="active">Work</Link>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
-import React from "react"
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import profilePic from '../img/profile-pic.png';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => {
+  return (
+    <Layout>
+      <div className="home-container">
+        <div className="profile-pic-container">
+          <img
+            alt="Profile Photo"
+            src={profilePic}
+            className="profile-pic"
+          />
+        </div>
+        <div className="home-text-container">
+          <h2>Hi! I&apos;m Anne!</h2>
+          <p>
+            I&apos;m a rising junior at Dartmouth College from Boston, MA.
+            I&apos;m studying Computer Science and Linguistics
+            and am passionate about leveraging technology for social impact.
+          </p>
+          <Link to="/projects">
+            <button type="button" className="work-btn">Check out my work!</button>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+};
