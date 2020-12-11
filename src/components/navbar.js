fbfd02c13763822bbbbbe1d0a8b24d1a3a8e326c
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2020-12-11 14:10:03 -0500

    navbar collapse on mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import '../styles/navbar.css';
+import hamburger from '../img/hamburger.png';
 
-const Navbar = () => {
+const MyNavbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
-    <div className="navbar">
-      <Link to="/" className="navlink name">Anne Bailey</Link>
-      <div className="navlinks">
-        <Link to="/" className="navlink" activeClassName="active">Home</Link>
-        <Link to="/projects/" className="navlink" activeClassName="active">Projects</Link>
-        <Link to="/work/" className="navlink" activeClassName="active">Work</Link>
+    <div>
+      <div className="navbar">
+        <Link to="/" className="navlink name">Anne Bailey</Link>
+        <div className="navlinks">
+          <Link to="/" className="navlink" activeClassName="active">Home</Link>
+          <Link to="/projects/" className="navlink" activeClassName="active">Projects</Link>
+          <Link to="/work/" className="navlink" activeClassName="active">Work</Link>
+        </div>
+        <div className="hamburger" onClick={() => setNavOpen(!navOpen)}>
+          <img
+            src={hamburger}
+            className="hamburger-img"
+          />
+        </div>
       </div>
+      {navOpen 
+      && (
+        <div className="dropdownLinks">
+          <Link to="/" className="navlink dropdownLink" activeClassName="active">Home</Link>
+          <Link to="/projects/" className="navlink dropdownLink" activeClassName="active">Projects</Link>
+          <Link to="/work/" className="navlink dropdownLink" activeClassName="active">Work</Link>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Navbar;
+export default MyNavbar;
